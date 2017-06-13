08ac48c3a3a8e0d13146cc85cb5a7c2ea7b5cf49
Travis Scott <traviss@iqmetrix.com>
2017-06-12 19:32:42 -0500

    fixed naming

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ io.on('connection', function(socket){
         console.log(err);
         io.emit('server:sendMessage', {
           conversationId: data.conversationId,
-          success,
+          message: success,
         });
 
       });
